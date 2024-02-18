f0a27a974b49ae838d30fcdd3ec79acc9f2b4fbe
CarinaWrightOgbat <12481630-CarinaWrightOgbat@users.noreply.replit.com>
2024-02-18 02:13:34 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+/* Students: Please use this week's project for Week 5: Assignment 5: Shared Libraries. 
+     You will need to replace the contents of this JavaScript file with your own work, 
+     and create any other files, if any, required for the assignment.
+     When you are done, be certain to submit the assignment in both Repl.it and Canvas to be graded. */
+
+let questions = [
+	{
+	number: 1,
+	question: "Is it a leap year?",
+	answers: ["Yes", "No"],
+	correct: 0
+},
+	{
+	number: 2,
+	question: "What year is it?",
+	answers: ["2023", "2024", "2004"],
+	correct: 1
+		},
+	{
+	number: 3,
+	question: "Do you like cats? (Yes there is a wrong and right answer",
+	answers: ["Yes", "No"],
+	correct: 0
+			}]
